28458a96f5a428580a602005051878a7da7bd71d
PirateNahid <piratenahid46@gmail.com>
2022-09-16 10:42:05 +0600

    gg

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iqtest/models/question_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +9,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+
+  List<Question> questions = [
+    Question(id: '10', title: 'title', options: {
+      '5':false,
+      '10':true
+    }),
+    Question(id: '11', title: 'title11', options: {
+      '6':false,
+      '11':true
+    })
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
